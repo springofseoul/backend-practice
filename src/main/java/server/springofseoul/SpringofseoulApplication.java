4ed32506e5d0907755fbff57f5ee0267e7558765
신재웅 (JaeWoong Shin) <104746510+woong2e@users.noreply.github.com>
2025-11-14 02:37:35 +0900

    [feat]: spring boot 프로젝트 초기 설정을 진행한다 (#2)
    * [feat]: spring boot 초기 프로젝트 설정을 진행한다 (#1)
    
    * [chore]: issue/PR 템플릿을 추가한다 (#1)
    
    * [docs]: springofseoul 프로젝트 규칙을 정리한다 (#1)

---
+++ src/main/java/server/springofseoul/SpringofseoulApplication.java
@@ -0,0 +1,20 @@
+package server.springofseoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringofseoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringofseoulApplication.class, args);
+        System.out.println("Hello World!!!!!!!ㅋㅋ");
+        System.out.println("Hello WKyoung-Minji");
+        System.out.println("Hello hongjeonghui");
+        System.out.println("Hello karneva");
+        System.out.println("Hello softdrink2025");
+        System.out.println("Hello SY_AHN");
+        System.out.println("Hello 신재웅 (JaeWoong Shin)");
+    }
+
+}
---
+++ src/test/java/server/springofseoul/SpringofseoulApplicationTests.java
@@ -0,0 +1,13 @@
+package server.springofseoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringofseoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
